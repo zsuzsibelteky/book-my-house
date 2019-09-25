e2feed3c41620edfa36bc620fff33475cd4ae657
mertinousse <martin@appmaintainers.com>
2019-09-25 13:37:46 +0200

    Remove booked helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,2 @@
 module ApplicationHelper
-  def booking_info(booked)
-    return '' if booked.nil?
-
-    if booked
-      'The house is booked for the selected day :('
-    else
-      'Yay! You can book the house on the selected day!'
-    end
-  end
 end
