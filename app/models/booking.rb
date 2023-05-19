a4a48d26911252e23956c052a1ce61a73dfb3235
zsuzsibelteky <zsuzsi.belteky@gmail.com>
2023-05-19 11:12:07 +0200

    Validation is implemented

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,6 +3,10 @@ class Booking < ApplicationRecord
 
   validates :day, presence: true, uniqueness: true
 
+  validates :number_of_guests, numericality: {less_than: 9}
+  validates :number_of_guests, numericality: {greater_than: 0}
+  validates :number_of_guests, numericality: {message: "must be between 1 and 8!"}
+
   scope :ordered, -> { order(:day) }
 
   def self.booked_for?(date)
